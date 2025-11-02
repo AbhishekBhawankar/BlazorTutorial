4c96b2e3dc4858cf4a026e357c068c83fe2d6274
Abhishek bhawankar <abhishekbhawankar@gmail.com>
2025-11-02 23:18:31 +0530

    sdf

--- BlazorTutorial/Program.cs
+++ BlazorTutorial/Program.cs
@@ -22,7 +22,7 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
+app.MapRazorComponents<App>() 
     .AddInteractiveServerRenderMode();
 
 app.Run();
